81d14f36c646f4b3955b997c809b5505a47faecc
manikanta-puppala <9704292046@gitam.in>
2021-06-11 11:34:50 +0530

    Python

---
+++ primenum.py
@@ -0,0 +1,13 @@
+i=int(input("Enter any number"))
+j=int(input("Enter any number"))
+p=i
+while i<=p<=j:
+    k=2
+    a=0
+    while k<p and a==0:
+        if p%k==0:
+            a=1
+        k=k+1
+    if a==0:
+        print(p)
+    p=p+1
